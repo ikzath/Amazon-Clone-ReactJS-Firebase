09e6db256098fd34122a485236c79e768dc54b9a
ikzath kalanthar <ikzathjob@hotmail.com>
2020-12-07 18:44:19 +0100

    Updates

---
+++ precache-manifest.fd6819b975bf565249df9509f56e1d5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05d371c6f41fd4ccae20308f0d5c68ce",
+    "url": "/ikzath/amazon-clone/index.html"
+  },
+  {
+    "revision": "982fa337d162dd2d1b1f",
+    "url": "/ikzath/amazon-clone/static/css/main.f7eb6741.chunk.css"
+  },
+  {
+    "revision": "b6b28ac3b8f0a929cb84",
+    "url": "/ikzath/amazon-clone/static/js/2.82a997e2.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/ikzath/amazon-clone/static/js/2.82a997e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "982fa337d162dd2d1b1f",
+    "url": "/ikzath/amazon-clone/static/js/main.dfa5c238.chunk.js"
+  },
+  {
+    "revision": "ca99a04992212669e778",
+    "url": "/ikzath/amazon-clone/static/js/runtime-main.b94fc28a.js"
+  }
+]);
